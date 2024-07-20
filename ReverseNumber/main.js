68f786fc8a81af918403c816c1cf1820273d340e
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-07-20 18:39:35 +0530

    added logic to reverse number

--- ReverseNumber/main.js
+++ ReverseNumber/main.js
@@ -1,6 +1,13 @@
 const reverseNumber = () => {
   let inputNumber = document.getElementById("inputNumber").value;
 
+  /* 
+  we can also achieve the same result using the simple logic below
+  but somehow we could do this way as well if inbuilt functions
+  usage is not allowed.
+  code is : Number(inputNumber.toString().split("").reverse().join(""));
+  */
+
   let rem,
     reversedNumber = 0;
 
