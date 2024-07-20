cbe141e9431879b34cebb56da9a944720d72a45c
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-07-20 10:49:32 +0530

    added logic to reverse every word in the sentence

--- ChangeColor/main.js
+++
@@ -1,5 +0,0 @@
-function changeColor() {
-  const colors = ["red", "blue", "green", "orange", "yellow", "aqua"];
-  let randomColor = Math.floor(Math.random() * (colors.length - 0)) + 0;
-  document.body.style.backgroundColor = colors[randomColor];
-}
