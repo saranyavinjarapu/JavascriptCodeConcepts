327de20c7bfcf7d16d6fc7df0c27bd0f3be6aa05
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-07-21 18:15:25 +0530

    logic to run most frequent item in the array

---
+++ MostFrequentItemInArray/main.js
@@ -0,0 +1,19 @@
+const mostFrequentItemInArray = () => {
+  let inputArray = document.getElementById("inputArray").value;
+  let strToArray = inputArray.split(",");
+  let freqArray = {};
+
+  strToArray.forEach((element) => {
+    if (freqArray.hasOwnProperty(element)) freqArray[element]++;
+    else freqArray[element] = 1;
+  });
+
+  let freqElem = Object.keys(freqArray).reduce((acc, num) =>
+    freqArray[acc] > freqArray[num] ? acc : num
+  );
+
+  console.log(freqElem);
+
+  document.getElementById("mostFrequentItemInArray").innerHTML =
+    "Most Frequent Item is : " + freqElem;
+};
